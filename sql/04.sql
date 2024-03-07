0a3a4741a9d6bd611e9a8b29370d240568e4dd6c
danzhechen <chendanzhe@gmail.com>
2024-03-06 19:34:19 -0800

    updated 04.sql

--- sql/04.sql
+++ sql/04.sql
@@ -3,19 +3,19 @@
  * 1. have appeared in at least one movie in the "Children" category,
  * 2. but that have never appeared in any movie in the "Horror" category.
  */
-SELECT a.first_name, a.last_name
-FROM actor a
-JOIN film_actor fa ON fa.actor_id = a.actor_id
-JOIN film f ON f.film_id = fa.film_id
-JOIN film_category fc ON fc.film_id = f.film_id
-JOIN category c ON c.category_id = fc.category_id
-WHERE c.name = 'Children'
+SELECT first_name, last_name
+FROM actor
+JOIN film_actor USING (actor_id)
+JOIN film USING (film_id)
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+WHERE name = 'Children'
 EXCEPT
-SELECT a.first_name, a.last_name
-FROM actor a
-JOIN film_actor fa ON fa.actor_id = a.actor_id
-JOIN film f ON f.film_id = fa.film_id
-JOIN film_category fc ON fc.film_id = f.film_id
-JOIN category c ON c.category_id = fc.category_id
-WHERE c.name = 'Horror'
+SELECT first_name, last_name
+FROM actor 
+JOIN film_actor USING (actor_id)
+JOIN film USING (film_id)
+JOIN film_category USING (film_id)
+JOIN category USING (category_id)
+WHERE name = 'Horror'
 ORDER BY last_name;
